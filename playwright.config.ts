f0408bde4db3a18c2765fe13cec3737909970159
Mohd Usmani <mohd.usmani@nagarro.com>
2026-06-10 11:05:13 +0530

    changes Done

--- playwright.config.ts
+++ playwright.config.ts
@@ -14,6 +14,7 @@ import { defineConfig, devices } from "@playwright/test";
 export default defineConfig({
   testDir: "./tests",
   /* Maximum time one test can run for. */
+  // await page.waitForTimeout(3000)
   timeout: 30 * 1000,
   expect: {
     /* Maximum time expect() should wait for the condition to be met.
