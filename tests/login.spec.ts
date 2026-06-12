cc4454c5c787e0b43c1d9567817d3d0d04475c7b
Mohd Usmani <mohd.usmani@nagarro.com>
2026-06-12 12:35:05 +0530

    test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from "../fixtures/pageFixtures";
 import config from "../config/configReader";
+import userData from "../test-data/userData.json";
 
 test.beforeEach(async ({ goToLoginPage }) => {
   await goToLoginPage.goToLoginPage();
