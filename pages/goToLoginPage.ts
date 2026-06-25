c0cbe58d8cb05723f9637abcceacfde16c64a8ee
Mohd Usmani <mohd.usmani@nagarro.com>
2026-06-25 17:46:32 +0530

    learning how to use .env file

--- pages/goToLoginPage.ts
+++ pages/goToLoginPage.ts
@@ -1,12 +1,14 @@
-import {Page, expect} from "@playwright/test";
+import {Locator, Page, expect} from "@playwright/test";
 import config from "../config/configReader";
 
 export class GoToLoginPage{
     readonly page : Page;
+    readonly openLoginPage;
     readonly pageOpenConfirmation;
 
     constructor(page:Page){
         this.page = page;
+        this.openLoginPage = page.getByRole("link",{name:"Form Authentication"});
         this.pageOpenConfirmation = page.getByText("Login Page");
     }
 
@@ -15,11 +17,12 @@ export class GoToLoginPage{
         // await this.page.goto(config.url);
 
         // Playwright expands this to: https://herokuapp.com
-        await this.page.goto('');
+        await this.page.goto('/');
+        await this.openLoginPage.click();
         
         //await this.page.goto('/forgot_password');  //If you ever add a page extension later, you only provide the sub-path
         // This verifies that the page URL strictly matches your global baseURL
-        await expect(this.page).toHaveURL(''); 
+        await expect(this.page).toHaveURL('/login'); 
 
         await expect(this.pageOpenConfirmation).toBeVisible();
     }
