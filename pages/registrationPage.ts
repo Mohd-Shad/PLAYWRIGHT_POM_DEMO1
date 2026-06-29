344610e59721b515b617ce4c614b90175bd0c151
Mohd Usmani <mohd.usmani@nagarro.com>
2026-06-29 18:34:42 +0530

    test

---
+++ pages/registrationPage.ts
@@ -0,0 +1,57 @@
+import {Page,expect} from "@playwright/test"
+import userData from "../test-data/userData.json"
+
+export class RegistrationPage{
+    readonly page;
+    readonly title
+    readonly heading;
+    readonly name_textbox;
+    readonly email_textbox;
+    readonly password_textbox;
+    readonly checkbox;
+    readonly dropdown;
+    readonly radio_button;
+    readonly dob;
+    readonly sub_button;
+    readonly success_message;
+
+    constructor(page:Page){
+        this.page = page;
+        this.title = page.title();
+        this.heading = page.getByRole("heading",{name:"Protractor Tutorial"});
+        this.name_textbox = page.locator('form input[name="name"]');
+        this.email_textbox = page.locator('form input[name="email"]');
+        this.password_textbox = page.getByRole("textbox",{name:"Password"});
+        this.checkbox = page.getByRole("checkbox",{name:"Check me out if you Love IceCreams!"});
+        this.dropdown = page.locator("#exampleFormControlSelect1"); //page.getByLabel("Gender")
+        this.radio_button = page.getByRole("radio",{name:"Student"});
+        this.dob = page.locator('input[name="bday"]');
+        this.sub_button = page.getByRole("button", {name:"Submit"});
+        this.success_message = page.getByText("Success! The Form has been submitted successfully!.");
+    }
+
+    async landingPage(){
+        await this.page.goto("https://rahulshettyacademy.com/angularpractice");
+    }
+    async verifyTitle(){
+        console.log(await this.title);
+        await expect(this.heading).toBeVisible();
+    }
+
+    async formFilling(){
+        await this.name_textbox.fill(userData.user1.name);
+        await this.email_textbox.fill(userData.user1.email);
+        await this.password_textbox.fill(userData.user1.password);
+        await this.checkbox.check();
+        await expect(this.checkbox).toBeChecked();
+        await this.dropdown.selectOption({label:"Male"});
+        await expect(this.dropdown).toHaveValue("Male");
+        await this.radio_button.check();
+        await expect(this.radio_button).toBeChecked();
+        await this.dob.fill("2000-04-15");
+        await this.sub_button.click();
+    }
+    async verifySuccessMessage(){
+        await expect(this.success_message).toBeVisible();
+    }
+}
--- tests/form-filling.spec.ts
+++ tests/form-filling.spec.ts
@@ -1,16 +1,27 @@
 import {test,expect} from "@playwright/test";
-
-test("Sample Form Filling", async ({page}) => {
-    await page.goto("https://rahulshettyacademy.com/angularpractice/");
+import { RegistrationPage } from "../pages/registrationPage";
 
 
-    await page.locator('form input[name="name"]').fill("Test User1");
-    await page.locator('//input[@name="email"]').fill("test1@yopmail.com");
-    await page.locator("#exampleInputPassword1").fill("Test12345@");
-    await page.locator("input#exampleCheck1").check();
-    await page.locator("id=exampleFormControlSelect1").selectOption({label:"Female"});
-    await page.locator('[id="inlineRadio2"]').check();
-    await page.locator('input[type="date"]').fill('2000-04-15');
-    await page.locator('input[type="submit"]').click();
-    
+test("Registration Form", async ({page})=>{
+    const registrationPage = new RegistrationPage(page);
+    await registrationPage.landingPage();
+    await registrationPage.verifyTitle();
+    await registrationPage.formFilling();
+    await registrationPage.verifySuccessMessage();
 })
+
+
+// test("Sample Form Filling", async ({page}) => {
+//     await page.goto("https://rahulshettyacademy.com/angularpractice/");
+
+
+//     await page.locator('form input[name="name"]').fill("Test User1");
+//     await page.locator('//input[@name="email"]').fill("test1@yopmail.com");
+//     await page.locator("#exampleInputPassword1").fill("Test12345@");
+//     await page.locator("input#exampleCheck1").check();
+//     await page.locator("id=exampleFormControlSelect1").selectOption({label:"Female"});
+//     await page.locator('[id="inlineRadio2"]').check();
+//     await page.locator('input[type="date"]').fill('2000-04-15');
+//     await page.locator('input[type="submit"]').click();
+    
+// })
